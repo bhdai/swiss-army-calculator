d61b614e8068d8d7fd71a1f639f7b5838fbec85b
Bui Dai <ss1280dzzz@gmail.com>
2025-06-09 21:09:46 +0700

    Initial commit: add main.cpp scaffold

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Swiss Army Calculator Initilized!" << std::endl;
+  // we will add more command parsing and functtion calls here lator
+  return 0;
+}
